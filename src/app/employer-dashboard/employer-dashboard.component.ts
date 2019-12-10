b5a187459f57f8fd1d511fa85b647bc308117146
Casey <clounsbur@gmail.com>
2019-12-10 16:31:25 -0500

    built employer-dashboard

--- src/app/employer-dashboard/employer-dashboard.component.ts
+++ src/app/employer-dashboard/employer-dashboard.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Employee } from "../interfaces/employee";
+import { ProfileService } from "../services/profile.service";
 
 @Component({
-  selector: 'app-employer-dashboard',
-  templateUrl: './employer-dashboard.component.html',
-  styleUrls: ['./employer-dashboard.component.css']
+  selector: "app-employer-dashboard",
+  templateUrl: "./employer-dashboard.component.html",
+  styleUrls: ["./employer-dashboard.component.css"]
 })
 export class EmployerDashboardComponent implements OnInit {
-
-  constructor() { }
+  constructor(private profileService: ProfileService) {}
+  employeeList: Employee[];
+  defaultAvatar: string = "../../assets/defaultAvatar.png";
 
   ngOnInit() {
+    this.employeeList = this.profileService.getEmployeeList();
   }
-
 }
